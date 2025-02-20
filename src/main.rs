d2c2d068850a3357947efa6df58e44404a14e6b9
Luis C. Gallego <tostadora@gmail.com>
2025-02-20 15:53:25 +0000

    changed the main to the correct Network

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ mod mnist;
 use network::Network;
 
 fn main() {
-   // let mut nw = Network::new(vec![2,4,3]);
+    let mut nw = Network::new(vec![784,30,10]);
 
     let training_data = match mnist::load_training_data() {
         Ok(t) => t,
